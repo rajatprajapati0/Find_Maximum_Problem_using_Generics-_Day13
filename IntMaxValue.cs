d6562d6f96a2306d382296f2441fae9a3f29e285
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-09 02:50:31 +0530

    Added:test for TC1.3

--- IntMaxValue.cs
+++ IntMaxValue.cs
@@ -12,7 +12,7 @@ namespace Find_Maximum_By_Genric
             if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0) { return secondValue; }
             if (thirdValue.CompareTo(firstValue)  > 0 && thirdValue.CompareTo(secondValue) > 0) { return thirdValue; }
            
-            return firstValue;
+            return thirdValue;
         }
 
     }
