bef8b32ac783da4ba24688c156016ccb1da3c382
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-09 02:18:04 +0530

    Added:Finding Maximum Problem using Generics program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Find_Maximum_By_Genric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
