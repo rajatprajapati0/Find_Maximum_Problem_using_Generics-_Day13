d26311dcaa8cd6c6fdb4dc900404294c20ca50a9
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-09 19:10:15 +0530

    Added:code refactored in Refactor1 by using genric mathod

--- MaxValue.cs
+++ MaxValue.cs
@@ -6,30 +6,13 @@ namespace Find_Maximum_By_Genric
 {
     public class MaxValue
     {
-        public int  IntMaxValue(int firstValue,int secondValue,int thirdValue) 
+        public T FindMaxValue<T>(T firstValue, T secondValue, T thirdValue) where T:IComparable
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0) { return firstValue; }
             if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0) { return secondValue; }
-            if (thirdValue.CompareTo(firstValue)  > 0 && thirdValue.CompareTo(secondValue) > 0) { return thirdValue; }
-           
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0) { return thirdValue; }
+
             return firstValue;
-        }
-
-        public float FloatMaxValue(float firstValue,float secondValue,float thirdValue) 
-        {
-            if (firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(secondValue)>0) { return firstValue; }
-            if (secondValue.CompareTo(thirdValue)>0 && secondValue.CompareTo(thirdValue)>0) { return secondValue; }
-            if (thirdValue.CompareTo(firstValue)>0 && thirdValue.CompareTo(secondValue)> 0){ return thirdValue; } 
-          
-            return secondValue;
-        }
-
-        public string stringMaxvalue(string firstValue, string secondValue, string thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue)> 0 && firstValue.CompareTo(thirdValue)  > 0) { return firstValue; }
-            if (secondValue.CompareTo(firstValue)> 0 && secondValue.CompareTo(thirdValue) > 0) { return secondValue; }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0) { return thirdValue; } 
-            return thirdValue;
         }
 
     }
--- Test.cs
+++ Test.cs
@@ -1,45 +1,58 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Find_Maximum_By_Genric
 {
-   public class Test
-   {
-        public void intTestForMax()
-        {
-            Console.WriteLine("enter three no. to find maximum number ");
-            int a=int.Parse(Console.ReadLine());
-            int b=int.Parse(Console.ReadLine());    
-            int c=int.Parse(Console.ReadLine());
-            MaxValue obj=new MaxValue();
-            int max=obj.IntMaxValue(a,b,c);
-            Console.WriteLine(max);
-        }
 
-        public void floatTestForMax()
+    public class Test
+    {
+        MaxValue maxValue;
+        public Test()
         {
-            Console.WriteLine("enter three no. to find maximum number ");
-            float a = float.Parse(Console.ReadLine());
-            float b = float.Parse(Console.ReadLine());
-            float c = float.Parse(Console.ReadLine());
-            MaxValue obj = new MaxValue();
-            float max = obj.FloatMaxValue(a,b,c);
-            Console.WriteLine(max);
+         maxValue=new MaxValue();
         }
-
-        public void stringTestFormax()
+        public void TestForMax()
         {
-            Console.WriteLine(" enter three name like apple,graeps,orenge ");
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
-            MaxValue obj = new MaxValue();
-            string max=obj.stringMaxvalue(a,b,c);
-            Console.WriteLine(max);  
+            Console.WriteLine("enter three value and find maximum ");
+            
+            int option=int.Parse(Console.ReadLine());
+            
 
+            switch (option)
+            {
+                case 1:
+                    var a = int.Parse(Console.ReadLine());
+                    var b = int.Parse(Console.ReadLine());
+                    var c = int.Parse(Console.ReadLine());
+                    Console.WriteLine(maxValue.FindMaxValue<int>( a, b , c));
+                    break;
+
+                case 2:
+                    var a2 = float.Parse(Console.ReadLine());
+                    var b2 = float.Parse(Console.ReadLine());
+                    var c2 = float.Parse(Console.ReadLine());
+                    Console.WriteLine( maxValue.FindMaxValue<float>(a2, b2, c2));
+                   
+                  
+                    break;
+
+                case 3:Console.WriteLine("enter name like apple,peach,banana,");
+                    var a1 = Console.ReadLine();
+                    var b1 = Console.ReadLine();
+                    var c1 = Console.ReadLine();
+                    Console.WriteLine(maxValue.FindMaxValue<string>(a1, b1, c1));
+                  
+                    break;
+
+                default:
+                    Console.WriteLine("enter correct option");
+                    break;
+
+            }
         }
-    
     }
 }
